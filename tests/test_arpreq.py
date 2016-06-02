908ebd289ca47ca16a17429e812805552f8b5c89
Sebastian Schrader <sebastian.schrader@tu-dresden.de>
2016-06-02 07:25:42 +0200

    Reimplement broken test

--- tests/test_arpreq.py
+++ tests/test_arpreq.py
@@ -1,32 +1,39 @@
-from socket import inet_ntoa
-from struct import unpack
-from unittest import TestCase
+import sys
+from socket import htonl, inet_ntoa
+
+import pytest
 
 from arpreq import arpreq
 
-class Test(TestCase):
-    def test_localhost(self):
-        self.assertEquals(arpreq('127.0.0.1'), '00:00:00:00:00:00');
 
-    @staticmethod
-    def decode_address(value):
-        return inet_ntoa(unpack('=I', bytes.fromhex(value)))
+def test_localhost():
+    assert arpreq('127.0.0.1') == '00:00:00:00:00:00'
 
-    @staticmethod
-    def decode_flags(value):
-        return unpack('=H', bytes.fromhex(value))
 
-    def get_default_gateway(self):
-        with open("/proc/net/route") as f:
-            for line in f:
-                fields = line.strip().split()
-                destination = self.decode_address(fields[1])
-                mask = self.decode_address(fields[7])
-                gateway = self.decode_address(fields[])
-                flags = self.decode_flags(fields[3])
-                if destination == '0.0.0.0' and mask == '0.0.0.0' and flags & 0x2:
-                    return gateway
+def decode_address(value):
+    return inet_ntoa(htonl(int(value, base=16)).to_bytes(4, 'big'))
 
-    def test_default_gateway(self):
-        gateway = get_default_gateway()
-        self.assertIsNotNone(arpreq(gateway))
+
+def decode_flags(value):
+    return int(value, base=16)
+
+
+def get_default_gateway():
+    with open("/proc/net/route") as f:
+        next(f)
+        for line in f:
+            fields = line.strip().split()
+            destination = decode_address(fields[1])
+            mask = decode_address(fields[7])
+            gateway = decode_address(fields[2])
+            flags = decode_flags(fields[3])
+            if destination == '0.0.0.0' and mask == '0.0.0.0' and flags & 0x2:
+                return gateway
+    return None
+
+
+def test_default_gateway():
+    gateway = get_default_gateway()
+    if not gateway:
+        pytest.skip("No default gateway present.")
+    assert arpreq(gateway) is not None

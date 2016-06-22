016608cb7e8837be679e435511168355b6cad78d
Sebastian Schrader <sebastian.schrader@tu-dresden.de>
2016-06-22 12:10:49 +0200

    Initialize mac_address

--- src/arpreq.c
+++ src/arpreq.c
@@ -214,6 +214,7 @@ arpreq(PyObject *self, PyObject *arg)
     bool error = false;
     bool found = false;
     struct sockaddr mac_address;
+    memset(&mac_address, 0, sizeof(mac_address));
     struct ifaddrs *head_ifa;
 
     Py_BEGIN_ALLOW_THREADS
